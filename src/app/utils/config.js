20be0ce5e20e2bddb98eecdad5169dcab965ab6f
xportas <xabierportasglz@gmail.com>
2024-07-24 17:38:51 +0200

    REFACT improve project organization

---
+++ src/app/utils/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  email: 'xabierportasglz@gmail.com',
+
+  links: {
+    GitHub: 'https://github.com/xportas',
+    LinkedIn: 'https://es.linkedin.com/in/xabierportas',
+    CV: '/cv-xportas.pdf'
+  },
+
+  navLinks: {
+    About: '#about-section',
+    Experience: '#expecience-section',
+    Work: '#work-section',
+    Contact: '#contact-section'
+    }
+};
