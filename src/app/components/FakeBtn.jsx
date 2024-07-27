1a04d56ff2ca41b13cf47d7c1cf9f6e5b3b5438f
xportas <105586880+xportas@users.noreply.github.com>
2024-07-27 20:45:18 +0200

    REFACT nav btns

--- src/app/components/FakeBtn.jsx
+++ src/app/components/FakeBtn.jsx
@@ -1,12 +1,28 @@
 'use client';
-import { email } from '../utils/config';
+import { useState } from 'react';
+import { waitForMs } from '../utils/utils';
 
-export default function FakeBtn({ text }) {
+export default function FakeBtn({ child, darkTheme, href, style, styles }) {
+
+  const [hoverBtnsEffectDisabled, setHoverBtnsEffectDisabled] = useState(true);
+
+  const handleClickOnNavBtns = async () => {
+    setHoverBtnsEffectDisabled(false);
+    await waitForMs(70);
+    setHoverBtnsEffectDisabled(true);
+  }
+
+
   return (
-    <a 
-    className=" no-count" 
-    href={`mailto:${email}`} >
-      { text }
+    <a
+      className={`${styles} ${darkTheme ? `bg-orange-200 border-main-gray shadow-custom-dark-theme` : `bg-main-gray border-orange-200 shadow-custom`} border-solid border-2 transition ease-in-out ${hoverBtnsEffectDisabled ? (darkTheme ? 'hover:shadow-custom-hover-dark-theme' : 'hover:shadow-custom-hover') + ' hover:-translate-y-1 hover:scale-105' : ''} no-count`}
+      style={style}
+      href={href}
+      onMouseDown={() => setHoverBtnsEffectDisabled(false)}
+      onMouseUp={() => setHoverBtnsEffectDisabled(true)}
+      onMouseLeave={() => setHoverBtnsEffectDisabled(true)}
+      onClick={handleClickOnNavBtns} >
+      {child}
     </a>
   );
 }
--- src/app/components/sections/Contact.jsx
+++ src/app/components/sections/Contact.jsx
@@ -1,4 +1,5 @@
 'use client';
+import { email } from '../../utils/config';
 import FakeBtn from '../FakeBtn';
 
 export default function Contact() {
@@ -15,11 +16,16 @@ export default function Contact() {
       <h2 className="text-clamp-title">
         Get In Touch
       </h2>
-      <p>
+      <p className='mt-6 mb-12'>
         Although I’m not currently looking for any new opportunities, my inbox is always open.
         Whether you have a question or just want to say hi, I’ll try my best to get back to you!
       </p>
-      <FakeBtn text={"Say Hello"} />
+      <FakeBtn
+      styles={'px-6 py-4'}
+      darkTheme={false}
+      href={`mailto:${email}`}
+      child={<span className='text-orange-200'>Say Hello</span>}
+      />
     </section>
   );
 }
