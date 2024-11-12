9360b7b4d51cfd5b650c16f992dc2e89f26f7cb3
xportas <xabierportasglz@gmail.com>
2024-11-12 19:20:25 +0100

    REFACT animations Projects component

--- src/app/sections/Projects.jsx
+++ src/app/sections/Projects.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import Plumber from '../components/Plumber';
 import { projects } from '../utils/config';
-import { dashedLine } from '../utils/utils';
+import { dashedLine, underlineEffect } from '../utils/utils';
 
 
 export default function Projects() {
@@ -55,15 +55,21 @@ export default function Projects() {
                 key={i}
                 className="relative grid gap-2.5 grid-cols-[repeat(12,1fr)] items-center mb-24 max-[768px]:mb-16 max-[480px]:mb-8"
               >
-                <div className="relative col-[1_/_7] row-span-full max-[1080px]:col-[1_/_9] max-[768px]:flex max-[768px]:flex-col max-[768px]:justify-center max-[768px]:h-full max-[768px]:col-span-full max-[768px]:z-[5] max-[768px]:pt-10 max-[768px]:pb-[30px] max-[768px]:px-10 max-[480px]:pt-[30px] max-[480px]:pb-5 max-[480px]:px-[25px]">
+                <div className="relative col-[1_/_7] row-span-full max-[1080px]:col-[1_/_9] max-[768px]:flex max-[768px]:flex-col max-[768px]:justify-center
+                                max-[768px]:h-full max-[768px]:col-span-full max-[768px]:z-[5] max-[768px]:pt-10 max-[768px]:pb-[30px] max-[768px]:px-10 
+                                max-[480px]:pt-[30px] max-[480px]:pb-5 max-[480px]:px-[25px]"
+                >
                   <div>
-                    <h6 className="text-secondary-orange text-[clamp(24px,5vw,28px)] font-header md:mt-0 md:mb-5 md:mx-0">
+                    <h6 className={`text-secondary-orange text-[clamp(24px,5vw,28px)] font-header md:mt-0 md:mb-5 md:mx-0 ${underlineEffect}`}>
                       <a href={github || '#'} target={"_blank"}>
                         {title}
                       </a>
                     </h6>
 
-                    <div className="relative z-20 p-6 rounded bg-main-gray text-orange-200 text-lg max-[768px]:text-main-gray max-[768px]:bg-transparent max-[768px]:shadow-none max-[768px]:px-0 max-[768px]:py-5 shadow-custom transition-all duration-[0.25s] ease-in">
+                    <div className="relative z-20 p-6 rounded bg-main-gray text-orange-200 text-lg max-[768px]:text-main-gray max-[768px]:bg-transparent
+                                    max-[768px]:shadow-none max-[768px]:px-0 max-[768px]:py-5 shadow-custom transition-all duration-[0.25s] ease-out 
+                                    hover:shadow-custom-hover hover:-translate-x-1 hover:-translate-y-1 focus:shadow-custom-hover focus:-translate-x-1 focus:-translate-y-1"
+                    >
                       <p>{description}</p>
                     </div>
 
@@ -75,7 +81,8 @@ export default function Projects() {
                         {tech.map((skill, j) => (
                           <li
                             key={j}
-                            className="text-secondary-gray max-[768px]:text-orange-800 text-[13px] whitespace-nowrap ml-0 mr-5 mt-0 mb-[5px] max-[768px]:text-[#ccd6f6)] max-[768px]:ml-0 max-[768px]:mr-2.5 max-[768px]:mt-0 max-[768px]:mb-[5px]"
+                            className="text-secondary-gray max-[768px]:text-orange-800 text-[13px] whitespace-nowrap ml-0 mr-5 mt-0 mb-[5px] 
+                                        max-[768px]:text-[#ccd6f6)] max-[768px]:ml-0 max-[768px]:mr-2.5 max-[768px]:mt-0 max-[768px]:mb-[5px]"
                           >
                             {skill}
                           </li>
@@ -87,17 +94,20 @@ export default function Projects() {
 
                 <div className="col-[6_/_-1] row-span-full relative z-[1] max-[768px]:col-span-full max-[768px]:h-full max-[768px]:opacity-30">
                   <a href={github || '#'} target={"_blank"} className={`relative z-10 w-full h-full rounded align-middle`}>
-                    <img 
+                    <img
                       src={cover} alt={title}
-                      className="shadow-custom rounded brightness-[70%] max-[768px]:object-cover max-[768px]:w-full max-[768px]:h-full max-[768px]:grayscale max-[768px]:contrast-100 max-[768px]:brightness-[50%]"
+                      className="shadow-custom rounded brightness-[70%] max-[768px]:object-cover max-[768px]:w-full max-[768px]:h-full
+                                max-[768px]:grayscale max-[768px]:contrast-100 max-[768px]:brightness-[50%] 
+                                transition-all duration-500 ease-out hover:shadow-custom-hover hover:-translate-x-1 hover:-translate-y-1
+                                focus:shadow-custom-hover focus:-translate-x-1 focus:-translate-y-1"
                     />
                   </a>
                 </div>
               </li>
             );
           })}
-          
-          <li><Plumber /></li> {/* TODO: DELETE when the rest of the projects are completed */}
+
+        <li><Plumber /></li> {/* TODO: DELETE when the rest of the projects are completed */}
 
       </ul>
     </section>
