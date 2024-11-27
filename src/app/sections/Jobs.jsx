4c4f6fc825c2dd3b7640edc25932d90375a9cafc
xportas <xabierportasglz@gmail.com>
2024-11-27 20:14:46 +0100

    FIX some section titles

--- src/app/sections/Jobs.jsx
+++ src/app/sections/Jobs.jsx
@@ -81,7 +81,7 @@ export default function Jobs({ screenWidth }) {
       className={`numbered max-[450px]:py-14 max-[768px]:py-20 md:py-24 transition-all duration-500 ease-in ${rendered ? 'opacity-100 blur-0' : 'opacity-0 blur-md'}`}
       id='experience'>
 
-      <h3 className={`flex items-center justify-center mb-7 mx-7 font-header text-responsive-section-heading ${screenWidth > 515 && dashedLine}`}>
+      <h3 className={`flex items-center justify-center mb-7 mx-3 font-header text-responsive-section-heading ${screenWidth > 770 && dashedLine}`}>
         Where I’ve Worked
       </h3>
 
--- src/app/sections/Projects.jsx
+++ src/app/sections/Projects.jsx
@@ -39,7 +39,7 @@ export default function Projects({ screenWidth }) {
                   ${rendered ? 'opacity-100 blur-0' : 'opacity-0 blur-md'} md:p-8`}
       id='work'
     >
-      <h3 className={`flex items-center justify-center mb-14 mx-7 font-header text-responsive-section-heading ${screenWidth > 768 && dashedLine}`}
+      <h3 className={`flex items-center justify-center mb-14 mx-3 font-header text-responsive-section-heading ${screenWidth > 768 && dashedLine}`}
       >
         Some Things I’ve Built
       </h3>
