bcf665ae11cc82c54bce94e5f731a806ba076252
xportas <xabierportasglz@gmail.com>
2024-12-31 18:37:15 +0100

    REFACT Loader Styles

--- src/app/components/Loader.jsx
+++ src/app/components/Loader.jsx
@@ -2,11 +2,17 @@
 export default function Loader({ progress }) {
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black z-[9999]" >
-      <div className="absolute top-1/2 left-1/2 text-center -translate-x-1/2 -translate-y-1/2 text-white" >
-        <p>Loading... {progress} %</p>
-        <div className="w-72 h-7 rounded-sm overflow-hidden border-4">
-          <div className={`bg-red-800 h-full`} style={{ width: `${progress}%`, transition: "width 0.3s ease" }} />
+    <div className="fixed top-0 left-0 w-full h-full bg-black z-[9999]">
+      <div className="absolute top-1/2 left-1/2 text-center -translate-x-1/2 -translate-y-1/2 text-white">
+        <p className="mb-3 animate-pulse">Loading...</p>
+        <div className="relative w-72 h-7 rounded-sm overflow-hidden border-4">
+          <div
+            className="bg-red-800 h-full"
+            style={{ width: `${progress}%`, transition: "width 0.3s ease" }}
+          />
+          <span className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-white">
+            {progress} %
+          </span>
         </div>
       </div>
     </div>
