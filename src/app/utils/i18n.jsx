fe6956015fba4125e397bab3ba7c5c3086dae99b
xportas <xabierportasglz@gmail.com>
2025-03-11 19:03:40 +0100

    ADD FEATURE auto-detect the user's language by the browser

--- src/app/utils/i18n.jsx
+++ src/app/utils/i18n.jsx
@@ -1,31 +1,30 @@
 import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import ES from '../../../public/locales/es/strings.json';
 import EN from '../../../public/locales/en/strings.json';
-import PT from '../../../public/locales/pt/strings.json';
+import ES from '../../../public/locales/es/strings.json';
 import GAL from '../../../public/locales/gal/strings.json';
+import PT from '../../../public/locales/pt/strings.json';
 
 const resources = {
-  es: {
-    strings: ES,
-  },
-  en: {
-    strings: EN,
-  },
-  pt: {
-    strings: PT,
-  },
-  gal: {
-    strings: GAL,
-  },
+  es: { strings: ES },
+  "es-ES": { strings: ES },
+  en: { strings: EN },
+  pt: { strings: PT },
+  gl: { strings: GAL },
+  gal: { strings: GAL },
 }
 
 i18next
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
     resources,
-    lng: 'en',
-    debug: false,
+    debug: true,
+    detection: {
+      order: ['navigator', 'htmlTag', 'querystring', 'cookie', 'localStorage', 'sessionStorage'],
+      caches: ['cookie', 'localStorage'],
+    },
     fallbackLng: 'en',
     saveMissing: true
   });
